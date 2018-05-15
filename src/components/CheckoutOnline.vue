9b310fe21272ad8ffcedce39e0df08751343cda7
LeoB_O <liubobotb@126.com>
2018-05-15 13:07:03 +0800

    update: checkout online

--- src/components/CheckoutOnline.vue
+++ src/components/CheckoutOnline.vue
@@ -6,6 +6,7 @@
       </div>
       <div class="address__detail">
         {{address.district+address.street}}
+        <span class="glyphicon glyphicon-menu-right" style="font-weight: 100;"></span>
       </div>
       <div class="address__name-phone">
         {{address.phone+' '+address.name}}
@@ -13,28 +14,67 @@
     </div>
     <hr>
     <div class="orders">
-      <div class="order" v-for="order in orders" :key="order.id">
-        <img class="order__img" :src="order.img">
-        <div class="order__name">
-          {{order.name}}
+      <div v-for="order in orders" :key="order.id">
+        <div class="order">
+          <img class="order__img" :src="order.img">
+          <div class="order__name">
+            {{order.name}}
+          </div>
+          <div class="order__amount">
+            x {{order.amount}}
+          </div>
+          <div class="order__origin-price">
+            ￥{{order.originPrice}}
+          </div>
+          <div class="order__sale-price">
+            ￥{{order.salePrice}}
+          </div>
         </div>
-        <div class="order__amount">
-          {{order.amount}}
+        <hr style="margin: 0px;">
+      </div>
+      <div class="orders__discount-notice">
+        <!--TODO 使用模板表示消息？ -->
+        满减活动不与单品同享
+      </div>
+      <hr>
+      <div class="orders__discount">
+        <div class="orders__discount-title">
+          优惠
         </div>
-        <div class="order__origin-price">
-          {{order.originPrice}}
+        <div class="orders__discount-detail">
+          <!--TODO 使用模板表示-->
+          优惠详情
+          <span class="glyphicon glyphicon-menu-right" style="font-weight: 100;"></span>
         </div>
-        <div class="order__sale-price">
-          {{order.salePrice}}
-        </div>
+      </div>
+      <hr>
+      <div class="orders__total">
+        <span class="orders__total-notice">
+          小计￥
+        </span>
+        <span class="orders__total-price">
+          {{total}}
+        </span>
       </div>
     </div>
     <div class="ordernote">
-      Here is order note
+      <span class="ordernote__title">
+        订单备注
+      </span>
+      <span class="ordernote__detail">
+        <!-- TODO 改成模板-->
+        <span>口味、偏好</span>
+        <span class="glyphicon glyphicon-menu-right" style="font-weight: 100;"></span>
+      </span>
     </div>
     <div style="height: 10%;"></div>
     <div class="footer">
-      Here is footer
+      <div class="footer__total">
+        ￥{{total}}
+      </div>
+      <div class="footer__pay">
+        支付
+      </div>
     </div>
   </div>
 </template>
@@ -70,11 +110,24 @@ export default {
         img: require('../assets/shop.png')
       }]
     }
+  },
+  computed: {
+    total: function () {
+      let sum = 0
+      for (let order of this.orders) {
+        sum += order.salePrice * order.amount
+      }
+      return sum
+    }
   }
 }
 </script>
 
 <style scoped>
+  hr {
+    margin: 0px;
+  }
+
   .checkout-online {
     height: 100%;
     width: 100%;
@@ -82,10 +135,58 @@ export default {
 
   .address {
     width: 100%;
+    padding: 10px;
+    text-align: left;
+  }
+
+  .address__notice {
+    color: lightgrey;
+  }
+
+  .address__detail {
+    font-size: large;
+    font-weight: bold;
+    margin: 5px 0px;
+  }
+
+  .address__name-phone {
+    font-weight: 400;
   }
 
   .orders {
     width: 100%;
+  }
+
+  .orders__discount-notice {
+    color: lightgrey;
+    margin: 10px 10px;
+  }
+
+  .orders__discount {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 15px;
+  }
+
+  .orders__discount-title {
+    font-size: large;
+    font-weight: 600;
+  }
+
+  .orders__discount-detail {
+  }
+
+  .orders__total {
+    text-align: right;
+    padding: 10px 10px;
+  }
+
+  .orders__total-notice {
+  }
+
+  .orders__total-price {
+    font-size: 20px;
+    font-weight: bold;
   }
 
   .order {
@@ -93,6 +194,7 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin: 0px 10px;
   }
 
   .order__img {
@@ -110,19 +212,61 @@ export default {
 
   .order__origin-price {
     width: 20%;
+    text-decoration: line-through;
+    color: lightgrey;
   }
 
   .order__sale-price {
     width: 20%;
+    color: red;
   }
 
   .ordernote {
+    padding: 10px 10px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
 
+  .ordernote__title {
+    font-weight: bold;
+  }
+
+  .ordernote__detail {
+    color: lightgrey;
   }
 
   .footer {
     position: fixed;
     bottom: 0px;
     height: 10%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  .footer__total {
+    font-size: 20px;
+    width: 70%;
+    height: 100%;
+    color: white;
+    background-color: #909090;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .footer__pay {
+    position: absolute;
+    right: 0px;
+    background-color: lawngreen;
+    width: 30%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    font-weight: bolder;
+    color: white;
   }
 </style>
